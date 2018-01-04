a8aabdb07f134d580454bdb2454fa8a8e3665201
Kyle MacDonald <kyle.macdonald@stanford.edu>
2018-01-04 13:16:08 -0800

    fixed bug in info gain model R code

--- R/information_gain_model.R
+++ R/information_gain_model.R
@@ -29,27 +29,29 @@ info_gain_answer <- function(prior_ent, cond_probs) {
 
 info_gain_question <- function(prior_ent, info_gain_answer_vect, prob_answer_vect) {
   posterior_ent <- (info_gain_answer_vect * prob_answer_vect) %>% sum()
-  prior_ent - posterior_ent
+  round(posterior_ent, digits = 3) # should we taking the difference between prior and posterior entropy here?
 }
 
 ################### Toy example
 
 ## setting up the problem, or what do we know about the world
+
+# prior prob of glom
 p_glom <- 0.7
+p_notglom <- 1 - p_glom
+
+# probs related to the hula question
 p_hula <- .34
 p_nothula <- 1 - p_hula
 p_hula_glom <- 0.1
 p_hula_fizo <- 0.9
-p_tea_glom <- 0.3
-p_tea_fizo <- 0.5
-
 
 ##### compute prior entropy. this captures everything the learner knows and how well they know it
-prior_ent_learner <- compute_entropy(probs_vect = c(p_glom, 1-p_glom))
+prior_ent_learner <- compute_entropy(probs_vect = c(p_glom, p_notglom))
 
 #### compute information gain if the the answer was "yes" hula wor
 cond_prob_glom_hula <- bayes_fun(p_glom, p_hula, p_hula_glom)
-cond_prob_fizo_hula <- bayes_fun(1 - p_glom, p_hula, p_hula_fizo)
+cond_prob_fizo_hula <- bayes_fun(p_notglom, p_hula, p_hula_fizo)
 
 
 ig_hula_answer <- info_gain_answer(prior_ent = prior_ent_learner,
@@ -57,14 +59,45 @@ ig_hula_answer <- info_gain_answer(prior_ent = prior_ent_learner,
 
 ## now do the same IG computation if the answer was "no" hula worn
 cond_prob_glom_nothula <- bayes_fun(p_glom, p_nothula, 1 - p_hula_glom)
-cond_prob_fizo_nothula <- bayes_fun(1 - p_glom, p_nothula, 1 - p_hula_fizo)
+cond_prob_fizo_nothula <- bayes_fun(p_notglom, p_nothula, 1 - p_hula_fizo)
 
 
 ig_nothula_answer <- info_gain_answer(prior_ent = prior_ent_learner,
-                                      cond_probs = c(cond_prob_glom_nothula, cond_prob_fizo_nothula))
+                                      cond_probs = c(cond_prob_glom_nothula, 
+                                                     cond_prob_fizo_nothula))
 
 ##### compute the expcted information gain for the hula question
 ## this is the step where we weight the utility (IG) of each answer by the probability of occurrence
 info_gain_question(prior_ent = prior_ent_learner,
                    info_gain_answer_vect = c(ig_hula_answer, ig_nothula_answer),
                    prob_answer_vect = c(p_hula, p_nothula))
+
+
+#### Now do the same process for the drinks tea question
+p_tea <- .36
+p_nottea <- 1 - p_tea
+p_tea_glom <- 0.3
+p_tea_fizo <- 0.5
+
+#### compute information gain if the the answer was "yes" tea 
+cond_prob_glom_tea <- bayes_fun(p_glom, p_tea, p_tea_glom)
+cond_prob_fizo_tea <- bayes_fun(p_notglom, p_tea, p_tea_fizo)
+
+ig_tea_answer <- info_gain_answer(prior_ent = prior_ent_learner,
+                                   cond_probs = c(cond_prob_glom_tea, 
+                                                  cond_prob_fizo_tea))
+
+
+#### compute information gain if the the answer was "no" tea 
+cond_prob_glom_nottea <- bayes_fun(p_glom, p_nottea, 1 - p_tea_glom)
+cond_prob_fizo_nottea <- bayes_fun(p_notglom, p_nottea, 1 - p_tea_fizo)
+
+ig_nottea_answer <- info_gain_answer(prior_ent = prior_ent_learner,
+                                      cond_probs = c(cond_prob_glom_nottea, 
+                                                     cond_prob_fizo_nottea))
+
+##### compute the expcted information gain for the tea question
+## this is the step where we weight the utility (IG) of each answer by the probability of occurrence
+info_gain_question(prior_ent = prior_ent_learner,
+                   info_gain_answer_vect = c(ig_tea_answer, ig_nottea_answer),
+                   prob_answer_vect = c(p_tea, p_nottea))
